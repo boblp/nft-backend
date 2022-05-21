8ac91056a047d0dfb449d60d69f1523542207a4d
BobXplosion <bob0011@hotmail.com>
2022-05-21 02:00:10 -0500

    Basic Pool Endpoints

--- app.js
+++ app.js
@@ -1,42 +1,80 @@
 const express = require('express')
+const { MongoClient, ObjectId } = require('mongodb')
 const app = express()
 const port = 3000
 const token = '8112557887258041'
+const uri = "mongodb+srv://boblp:328bu5ad@cluster0.lz6dt.mongodb.net/?retryWrites=true&w=majority"
+let db
+
+MongoClient.connect(uri, function(err, database) {
+  if(err) return console.error(err)
+  db = database.db('nfts')
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
 
 const quickAuth = (req, res, next) => {
-  if (req.query.authToken !== token) {
-    res.send('Wrong Token')
-  }
+  if (req.query.authToken !== token) res.send('Wrong Token')
 
   next()
 }
 
 app.use(quickAuth)
 
+app.get('/pools/', (req, res) => {
+  db.collection('pools').find().toArray((err, docs) => {
+    if (err) console.error(err)
+    res.send(docs)
+  })
+})
+
 app.get('/pools/:poolId', (req, res) => {
-  if(req.params.poolId){
-    console.log('INCLUDE ID IN QUERY')
+  if (!req.params.poolId) res.send('INCLUDE ID IN QUERY')
+  const _id = new ObjectId(req.params.poolId)
+
+  db.collection('pools').find({ _id }).toArray((err, docs) => {
+    if (err) console.error(err)
+    res.send(docs[0])
+  })
+})
+
+app.post('/pools', (req, res) => {
+  if (!req.query.name) res.send('DATA MISSING')
+
+  const obj = {
+    name: req.query.name,
+    nfts: []
   }
 
-  res.send('RETURN QUERY')
+  db.collection('pools').insertOne(obj, (err, docs) => {
+    if (err) console.error(err)
+    res.send(docs)
+  })
 })
 
-app.post('/pools', (req, res) => {  
-  res.send('CREATE EMPTY NEW POOL, RETURN ALL POOLS')
+app.patch('/pools/:poolId', (req, res) => {
+  const { poolId } = req.params
+  const nfts = JSON.parse(req.query.nfts)
+
+  if(!poolId || !nfts) return res.send("PoolId is required")
+
+  const _id = new ObjectId(poolId)
+  const update = { $set: nfts }
+
+  db.collection('pools').updateOne({ _id }, update, (err, docs) => {
+    if (err) console.error(err)
+    res.send(docs)
+  })
 })
 
-app.patch('/pool/:poolId', (req, res) => {
-  if(!req.params.poolId) return res.send("PoolId is required")
+app.delete('/pools/:poolId', (req, res) => {
+  if (!req.params.poolId) res.send('DATA MISSING')
+  const _id = new ObjectId(req.params.poolId)
 
-  res.send('UPDATE POOL NFTS AND RETURN NEW')
-})
-
-app.delete('/pool/:poolId', (req, res) => {
-  if(!req.params.poolId) return res.send("PoolId is required")
-
-  res.send('DELETE POOL')
+  db.collection('pools').deleteOne({ _id }, (err, docs) => {
+    if (err) console.error(err)
+    res.send(docs)
+  })
 })
